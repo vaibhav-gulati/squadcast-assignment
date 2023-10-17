33b9849ace50a882b9b9b5f2baad986bb4d1e6e3
vaibhav-gulati <vaibhavgulati750@gmail.com>
2023-10-17 14:39:32 +0530

    props modification and reusability

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import MentionUser from './components/MentionUser'
-import './App.css'
+import './App.css';
+import mentionsData from './assets/data.json';
+import { useState } from 'react';
+
+
+interface userProp{
+  mentions: string;
+}
+
 
 function App() {
 
+  const [users,setUsers]=useState('');
+
+  const handleUSers=(user)=>{
+    console.log("user name",user)
+  }
+
   return (
     <>
-     <MentionUser />
+     <MentionUser mentions = {mentionsData} onChange={handleUSers}/>
     </>
   )
 }
--- src/components/MentionUser.tsx
+++ src/components/MentionUser.tsx
@@ -1,17 +1,23 @@
 import '../App.css'; 
 import React, { useState } from 'react';
 
-import mentionsData from '../assets/data.json';
+// import mentionsData from '../assets/data.json';
 
-const MentionUser: React.FC = () => {
+interface userProp{
+  mentions: string[];
+  onChange: (user: string)=>void;
+}
+
+const MentionUser: React.FC<userProp>= (props) => {
   const [inputValue, setInputValue] = useState<string>('');
   const [mentions, setMentions] = useState<string[]>([]);
   const [showMentionOptions, setShowMentionOptions] = useState<boolean>(false);
 
-  const mentionOptions: string[] = mentionsData.mentions.sort();
+  const mentionOptions: string[] = props.mentions.mentions.sort();
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   const text = event.target.value;
+
   setInputValue(text);
   const mentionIndex = text.lastIndexOf('@');
 
@@ -27,9 +33,24 @@ const MentionUser: React.FC = () => {
 
   const handleSelectMention = (index: number) => {
     const selectedMention = mentionOptions[index];
-    const mentionIndex = inputValue.indexOf('@');
-    const updatedValue =
-      inputValue.slice(0, mentionIndex) + `@${selectedMention} ` + inputValue.slice(mentionIndex + 1);
+    const mentionIndex = inputValue.lastIndexOf('@');
+    props.onChange(`@${mentionOptions[index]}`)
+
+    let updatedValue=''
+
+    if(mentionIndex!==-1){
+      const prevVal = inputValue.slice(0, mentionIndex);
+      const nextVal = inputValue.slice(mentionIndex + 1);
+
+      updatedValue = prevVal + `@${selectedMention} ` + nextVal
+    }
+    else{
+      updatedValue=inputValue+`@${selectedMention} `;
+    }
+
+
+    //  updatedValue =
+    //   inputValue.slice(0, mentionIndex) + `@${selectedMention} ` + inputValue.slice(mentionIndex + 1);
     setInputValue(updatedValue);
     setShowMentionOptions(false);
     setMentions([...mentions, selectedMention]);
